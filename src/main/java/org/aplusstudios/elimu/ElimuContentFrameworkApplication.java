948d8e2914ebd8d64240c264b62ec58bd4241640
samuelowino <samuelowino43@yahoo.com>
2021-07-11 21:35:51 +0300

    Project init

---
+++ src/main/java/org/aplusstudios/elimu/ElimuContentFrameworkApplication.java
@@ -0,0 +1,13 @@
+package org.aplusstudios.elimu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElimuContentFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElimuContentFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/aplusstudios/elimu/ElimuContentFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aplusstudios.elimu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElimuContentFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
